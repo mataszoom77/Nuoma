2a78cab43a67ccf51c3e55a01392e525a167e195
FlipReck <100430692+FlipReck@users.noreply.github.com>
2022-12-19 22:23:26 +0200

    IrangaDone

--- redaguoti.php
+++ redaguoti.php
@@ -30,6 +30,18 @@ $row = $result->fetch_assoc()
     <title>Demo projektas</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="include/styles.css" rel="stylesheet" type="text/css">
+	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	
+	<script type="text/javascript">
+function confirmationDelete(anchor)
+{	   
+	alert("Įranga sėkmingai paredaguota");
+
+}
+</script>
+	
+	
 </head>
 
 <body>
@@ -66,18 +78,114 @@ $row = $result->fetch_assoc()
                             </div>
                             <div class="form-row">
                                 <div class="form-group col-md-6">
-                                    <label for="inputPassword4">Spalva</label>
-                                    <input type="text" name="spalva" value="<?php echo $row['spalva'] ?>" class="form-control" id="inputPassword4" required>
+                                    <label for="inputPassword4">Kaina</label>
+                                    <input type="text" name="kaina" value="<?php echo $row['kaina'] ?>" class="form-control" id="inputPassword4" required>
                                 </div>
 
                                 <div class="form-group col-md-6">
-                                    <label for="inputAddress">Svoris</label>
-                                    <input type="text" name="kaina" value="<?php echo $row['svoris'] ?>" class="form-control" id="inputAddress" required>
+                                    <label for="inputAddress">Kiekis</label>
+                                    <input type="text" name="kiekis" value="<?php echo $row['kiekis'] ?>" class="form-control" id="inputAddress" required>
+                                </div>
+                            </div>
+							
+							<div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputPassword4">Prieinamumas</label>
+                                    <input type="text" name="prieinamumas" value="<?php echo $row['prieinamumas'] ?>" class="form-control" id="inputPassword4" required>
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label for="inputAddress">Spalva</label>
+                                    <input type="text" name="spalva" value="<?php echo $row['spalva'] ?>" class="form-control" id="inputAddress">
+                                </div>
+                            </div>
+							
+							<div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputPassword4">Svoris</label>
+                                    <input type="text" name="svoris" value="<?php echo $row['svoris'] ?>" class="form-control" id="inputPassword4">
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label for="inputAddress">Dydis</label>
+                                    <input type="text" name="dydis" value="<?php echo $row['dydis'] ?>" class="form-control" id="inputAddress">
+                                </div>
+                            </div>
+							
+							<div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputPassword4">Ilgis</label>
+                                    <input type="text" name="ilgis" value="<?php echo $row['ilgis'] ?>" class="form-control" id="inputPassword4">
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label for="inputAddress">Nusidevėjimas</label>
+                                    <input type="text" name="nusidevejimas" value="<?php echo $row['nusidevejimas'] ?>" class="form-control" id="inputAddress">
+                                </div>
+                            </div>
+							
+							<div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputPassword4">Prekės ženklas</label>
+                                    <input type="text" name="prekes_zenklas" value="<?php echo $row['prekes_zenklas'] ?>" class="form-control" id="inputPassword4">
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label for="inputAddress">Pagaminimo šalis</label>
+                                    <input type="text" name="pagaminimo_salis" value="<?php echo $row['pagaminimo_salis'] ?>" class="form-control" id="inputAddress">
+                                </div>
+                            </div>
+							
+							<div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputPassword4">Reguliuojamas dydis</label>
+                                    <input type="text" name="reguliuojamas_dydis" value="<?php echo $row['reguliuojamas_dydis'] ?>" class="form-control" id="inputPassword4">
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label for="inputAddress">Maksimalus greitis</label>
+                                    <input type="text" name="maksimalus_greitis" value="<?php echo $row['maksimalus_greitis'] ?>" class="form-control" id="inputAddress">
+                                </div>
+                            </div>
+							
+							<div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputPassword4">Maksimalus nuvažiuojamas atstumas</label>
+                                    <input type="text" name="maksimalus_nuvaziuojamas_atstumas" value="<?php echo $row['maksimalus_nuvaziuojamas_atstumas'] ?>" class="form-control" id="inputPassword4">
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label for="inputAddress">Akumuliatoriaus talpa</label>
+                                    <input type="text" name="akumuliatoriaus_talpa" value="<?php echo $row['akumuliatoriaus_talpa'] ?>" class="form-control" id="inputAddress">
+                                </div>
+                            </div>
+							
+							<div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputPassword4">Įkrovimo laikas</label>
+                                    <input type="text" name="ikrovimo_laikas" value="<?php echo $row['ikrovimo_laikas'] ?>" class="form-control" id="inputPassword4">
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label for="inputAddress">Stiprumas pagrindine ašimi</label>
+                                    <input type="text" name="stiprumas_pagrindine_asimi" value="<?php echo $row['stiprumas_pagrindine_asimi'] ?>" class="form-control" id="inputAddress">
+                                </div>
+                            </div>
+							
+							<div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputPassword4">Stiprumas pagalbine ašimi</label>
+                                    <input type="text" name="stiprumas_pagalbine_asimi" value="<?php echo $row['stiprumas_pagalbine_asimi'] ?>" class="form-control" id="inputPassword4">
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label for="inputAddress">Stiprumas atidarytais vartais</label>
+                                    <input type="text" name="stiprumas_atidarytais_vartais" value="<?php echo $row['stiprumas_atidarytais_vartais'] ?>" class="form-control" id="inputAddress">
                                 </div>
                             </div>
                             
                             <input type="hidden" name="id" value="<?php echo $row['id'] ?>">
-                            <button type="submit" name="update" value="Submit" class="btn btn-primary">Patvirtinti</button>
+                            <button type="submit" onclick='javascript:confirmationDelete($(this));' name="update" value="Submit" class="btn btn-primary">Patvirtinti</button>
                         </form>
                         <!-- <?php
                                 //echo "<br><br><a href=\"index.php\"><b>Grįžti į meniu<b></a>";
